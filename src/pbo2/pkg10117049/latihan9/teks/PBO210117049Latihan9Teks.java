ccab73b44f7b8867c7b71fa3ee659b78be45c37f
Gery Akbar Fauzi <43455599+geryakbarf@users.noreply.github.com>
2018-09-22 10:23:09 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117049/latihan9/teks/PBO210117049Latihan9Teks.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117049.latihan9.teks;
+
+/**
+ *
+ * @author Gery Akbar Fauzi
+ * Nama                 : Gery Akbar Fauzi
+ * Kelas                : PBO2
+ * Nim                  : 10117049
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan jenis - 
+ *                        jenis tipe data teks 
+ */
+public class PBO210117049Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // deklarasi variabel dengan tipe data teks
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        System.out.println("Hasil "+ newline + "Char : "+ tab +" berisi "+ c);
+    }
+    
+}
